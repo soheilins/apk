07bcfa4676b2e1048c9682300a46a50fa6e623e4
soheilins <153309603+soheilins@users.noreply.github.com>
2026-05-19 06:53:16 +0330

    Update GitHubModels.kt

--- app/src/main/java/com/soheil/hailofblades/models/GitHubModels.kt
+++ app/src/main/java/com/soheil/hailofblades/models/GitHubModels.kt
@@ -1,2 +1,15 @@
 package com.soheil.hailofblades.models
-data class PlaceholderModel(val id: Int, val name: String)
+
+import com.google.gson.annotations.SerializedName
+
+data class WorkflowDispatchRequest(
+    @SerializedName("ref") val ref: String,
+    @SerializedName("inputs") val inputs: Map<String, String>
+)
+
+data class GitHubContent(
+    @SerializedName("name") val name: String,
+    @SerializedName("path") val path: String,
+    @SerializedName("content") val content: String,  // base64 encoded
+    @SerializedName("encoding") val encoding: String
+)
